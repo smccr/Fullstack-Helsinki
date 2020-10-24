6d6d0c1c9c07aed33ba501329a2734a3adb8d74c
Sergio <smccr123@gmail.com>
2020-10-24 01:57:16 -0600

    5.18: bloglist end to end testing, step2

--- part5/frontend/cypress/integration/blog_app.spec.js
+++ part5/frontend/cypress/integration/blog_app.spec.js
@@ -1,6 +1,9 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    cy.request('POST', 'http://localhost:3001/api/users', {
+      username: 'testuser', password: 'testpassword', name: 'Test User'
+    })
     cy.visit('http://localhost:3000')
   })
 
@@ -9,5 +12,24 @@ describe('Blog app', function() {
     cy.contains('username')
     cy.contains('password')
     cy.get('#login').contains('login')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('testuser')
+      cy.get('#password').type('testpassword')
+      cy.get('#login').click()
+
+      cy.contains('Test User logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('wronguser')
+      cy.get('#password').type('wrongpassword')
+      cy.get('#login').click()
+
+      cy.contains('Wrong credentials')
+      cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
