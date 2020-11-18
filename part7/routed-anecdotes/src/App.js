1f832dab948dff694779647e756b9e1d369893c4
Sergio <smccr123@gmail.com>
2020-11-17 20:30:18 -0600

    7.6: anecdotes and hooks step3

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -72,17 +72,16 @@ const CreateNew = (props) => {
   const info = useField('text')
   const history = useHistory()
 
-
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content: content.value,
-      author: author.value,
-      info: info.value,
+      content: content.input.value,
+      author: author.input.value,
+      info: info.input.value,
       votes: 0
     })
     history.push('/')
-    props.notify(content.value)
+    props.notify(content.input.value)
   }
 
   const reset = () => {
@@ -90,22 +89,22 @@ const CreateNew = (props) => {
     author.reset()
     info.reset()
   }
-
+  
   return (
     <div>
       <h2>create a new anecdote</h2>
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input {...content} />
+          <input {...content.input} />
         </div>
         <div>
           author
-          <input {...author} />
+          <input {...author.input} />
         </div>
         <div>
           url for more info
-          <input {...info} />
+          <input {...info.input} />
         </div>
         <button>create</button>
       </form>
--- part7/routed-anecdotes/src/hooks/index.js
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -12,9 +12,11 @@ export const useField = (type) => {
   }
 
   return {
-    type,
-    value,
-    onChange,
-    reset
+    reset,
+    input: {
+      type,
+      value,
+      onChange
+    }
   }
 }
