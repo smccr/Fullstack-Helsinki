a4ba976d3fefdec1de368e41f91615c470c702b0
Sergio <smccr123@gmail.com>
2021-03-15 19:20:10 -0600

    8.14: Database, part 2

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -17,83 +17,6 @@ mongoose.connect(process.env.MONGODB_URI, {
     console.log('error connecting to MongoDB:', error.message)
   })
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  {
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  {
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
-
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
 
 const typeDefs = gql`
     type Book {
@@ -168,7 +91,10 @@ const resolvers = {
     name: (root) => root.name,
     born: (root) => root.born,
     id: (root) => root.id,
-    bookCount: (root) => books.filter(b => b.author === root.name).length
+    bookCount: async (root) => {
+      const booksByAuthor = await Book.find({ author: { $in: root.id }});
+      return booksByAuthor.length;
+    } 
   },
   Mutation: {
 
@@ -186,14 +112,17 @@ const resolvers = {
       return await book.save();
     },
 
-    editAuthor: (root, args) => {
-      const findAuthor = authors.find(a => a.name === args.name)
-      if (findAuthor) {
-        const modifiedAuthor = { ...findAuthor, born: args.setBornTo }
-        authors = authors.map(a => a.name === findAuthor.name ? modifiedAuthor : a)
-        return modifiedAuthor
+    editAuthor: async (root, args) => {
+      const name = args.name;
+      const born = args.setBornTo;
+      const author = await Author.findOne({ name })
+      if(author) {
+        const newAuthor = { name, born }
+        const modifiedAuthor = await Author.findByIdAndUpdate(author._id, newAuthor, { new: true });
+        return modifiedAuthor;
+      } else {
+        return null;
       }
-      return null
     }
   }
 }
