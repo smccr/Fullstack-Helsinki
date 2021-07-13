92626ee3bcd04c9123f07127597b861eff0e630c
Sergio <smccr123@gmail.com>
2021-07-12 23:24:14 -0600

    9.10: Patientor backend, step3

--- part9/exercises 9.8-9.13/express/src/index.ts
+++ part9/exercises 9.8-9.13/express/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 const app = express();
+import diagnoseRouter from './routes/diagnoses';
+
 import cors from 'cors';
 
 const options : cors.CorsOptions = {
@@ -9,10 +11,13 @@ const options : cors.CorsOptions = {
 app.use(cors(options));
 app.use(express.json());
 
+app.use('/api/diagnoses', diagnoseRouter);
+
 app.get('/api/ping', (_req, res) => {
   console.log('ping');
   return res.send("<h1>Hello</h1>");
 });
+
 
 const PORT = 3001;
 
