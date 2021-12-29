16502a4cd2dc4ff4faea5b696f5392875ed9df7b
Sergio <smccr123@gmail.com>
2021-12-28 19:05:18 -0600

    Task 13.7

--- part13/controllers/blogs.js
+++ part13/controllers/blogs.js
@@ -21,15 +21,11 @@ router.get('/:id', blogFinder, async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  try {
     const body = req.body;
     const newBlog = { ...body, date: Date.now() };
     const blog = await Blog.create(newBlog);
 
     return res.json(blog);
-  } catch (error) {
-    return res.status(400).json({ error });
-  }
 });
 
 router.delete('/:id', blogFinder, async (req, res) => {
